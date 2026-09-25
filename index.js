53cbc022e7148c87440845786a3e77d79b51265c
Siddharth-here <siddharthmohapatra2004@gmail.com>
2026-09-25 20:43:36 +0530

    socket connection established

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-
 import http from "node:http";
-import express from "express";
 import path from "node:path";
 
+import express from "express";
+import { Server } from "socket.io";
+
 async function main() {
-  const app = express();
-  const server = http.createServer(app);
   const PORT = process.env.PORT ?? 8000;
 
+  const app = express();
+  const server = http.createServer(app);
+
+  const io = new Server();
+  io.attach(server);
+
+  //Socket IO Handler
+  io.on("connection", (socket) => {
+    console.log(`socket connected`, { id: socket.id });
+  });
+
+  //Express Handler
   app.get("/health", (req, res) => res.json({ healty: true }));
 
   app.use(express.static(path.resolve("./public"))); //middleware tells if the files is in public folder then show it to user
-
-  
 
   server.listen(PORT, () => {
     console.log(`server is running on http://localhost:${PORT}`);
