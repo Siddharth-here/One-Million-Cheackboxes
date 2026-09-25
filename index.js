702acfb4fab2a147d1259488fffbd16c12ee16a3
Siddharth-here <siddharthmohapatra2004@gmail.com>
2026-09-25 19:28:39 +0530

    updated

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
+
 import http from "node:http";
 import express from "express";
+import path from "node:path";
 
 async function main() {
   const app = express();
-  const server = http.createServer();
+  const server = http.createServer(app);
   const PORT = process.env.PORT ?? 8000;
 
   app.get("/health", (req, res) => res.json({ healty: true }));
+
+  app.use(express.static(path.resolve("./public"))); //middleware tells if the files is in public folder then show it to user
+
+  
 
   server.listen(PORT, () => {
     console.log(`server is running on http://localhost:${PORT}`);
   });
 }
 
-main()
+main();
