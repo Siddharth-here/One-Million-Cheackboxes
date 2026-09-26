0bdb8b94a9a9997b2571288f70475b7b2f4ba7bc
Siddharth-here <siddharthmohapatra2004@gmail.com>
2026-09-26 19:52:25 +0530

    implement checkbox state synchronization with Redis subscriber

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import path from "node:path";
 
 import express from "express";
 import { Server } from "socket.io";
-import { stat } from "node:fs";
+import { publisher, subscriber } from "./redis-connection.js";
+import { channel } from "node:diagnostics_channel";
 
 const CHECKBOX_SIZE = 100;
 const state = {
@@ -19,14 +20,28 @@ async function main() {
   const io = new Server();
   io.attach(server);
 
+  subscriber.subscribe("internal-server:checkbox:change");
+  subscriber.on("message", (channel, message) => {
+    if (channel === "internal-server:checkbox:change") {
+      const { index, checked } = JSON.parse(message);
+      state.checkboxes[index] = checked;
+      io.emit("server:checkbox:change",{ index, checked });
+    }
+  });
+
   //Socket IO Handler
   io.on("connection", (socket) => {
     console.log(`socket connected`, { id: socket.id });
 
-    socket.on("client:checkbox:change", (data) => {
+    socket.on("client:checkbox:change", async (data) => {
       console.log(`[Socket:${socket.id}]:client:checkbox:change`, data);
-      io.emit("server:checkbox:change", data);
-      state.checkboxes[data.index] = data.checked;
+      // io.emit("server:checkbox:change", data);
+      // state.checkboxes[data.index] = data.checked;
+
+      await publisher.publish(
+        "internal-server:checkbox:change",
+        JSON.stringify(data),
+      );
     });
   });
 
