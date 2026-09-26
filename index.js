1a0b56dafaf137fa75433b6e0e90e163e095d220
Siddharth-here <siddharthmohapatra2004@gmail.com>
2026-09-26 19:12:52 +0530

    implement checkbox state synchronization with server

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ import path from "node:path";
 
 import express from "express";
 import { Server } from "socket.io";
+import { stat } from "node:fs";
+
+const CHECKBOX_SIZE = 100;
+const state = {
+  checkboxes: new Array(CHECKBOX_SIZE).fill(false),
+};
 
 async function main() {
   const PORT = process.env.PORT ?? 8000;
@@ -17,17 +23,21 @@ async function main() {
   io.on("connection", (socket) => {
     console.log(`socket connected`, { id: socket.id });
 
-    socket.on('client:checkbox:change', (data)=>{
-      console.log(`[Socket:${socket.id}]:client:checkbox:change`,data);
-      io.emit('server:checkbox:change',data)
-      
-    })
+    socket.on("client:checkbox:change", (data) => {
+      console.log(`[Socket:${socket.id}]:client:checkbox:change`, data);
+      io.emit("server:checkbox:change", data);
+      state.checkboxes[data.index] = data.checked;
+    });
   });
 
   //Express Handler
   app.get("/health", (req, res) => res.json({ healty: true }));
 
   app.use(express.static(path.resolve("./public"))); //middleware tells if the files is in public folder then show it to user
+
+  app.get("/checkboxes", (req, res) => {
+    return res.json({ checkboxes: state.checkboxes });
+  });
 
   server.listen(PORT, () => {
     console.log(`server is running on http://localhost:${PORT}`);
